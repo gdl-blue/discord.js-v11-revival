544ceb62ef985ab23a2c6691833d719c9c6418d8
turbo-whistler <gnote8080@gmail.com>
2020-12-04 10:22:48 +0900

    -node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /*
-  ONLY RUN BUILDS WITH `npm run web-dist`!
-  DO NOT USE NORMAL WEBPACK! IT WILL NOT WORK!
+    ONLY RUN BUILDS WITH `npm run web-dist`!
+    DO NOT USE NORMAL WEBPACK! IT WILL NOT WORK!
 */
 
 const webpack = require('webpack');
@@ -8,36 +8,36 @@ const createVariants = require('parallel-webpack').createVariants;
 const version = require('./package.json').version;
 
 const createConfig = (options) => {
-  const plugins = [
-    new webpack.DefinePlugin({ 'global.GENTLY': false }),
-  ];
+    const plugins = [
+        new webpack.DefinePlugin({ 'global.GENTLY': false }),
+    ];
 
-  if (options.minify) plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+    if (options.minify) plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
 
-  const filename = `./webpack/discord${process.env.VERSIONED === 'false' ? '' : '.' + version}${options.minify ? '.min' : ''}.js`; // eslint-disable-line
+    const filename = `./webpack/discord${process.env.VERSIONED === 'false' ? '' : '.' + version}${options.minify ? '.min' : ''}.js`; // eslint-disable-line
 
-  return {
-    entry: './src/index.js',
-    output: {
-      path: __dirname,
-      filename,
-    },
-    module: {
-      rules: [
-        { test: /\.md$/, loader: 'ignore-loader' },
-      ],
-    },
-    node: {
-      fs: 'empty',
-      dns: 'mock',
-      tls: 'mock',
-      child_process: 'empty',
-      dgram: 'empty',
-      zlib: 'empty',
-      __dirname: true,
-    },
-    plugins,
-  };
+    return {
+        entry: './src/index.js',
+        output: {
+            path: __dirname,
+            filename,
+        },
+        module: {
+            rules: [
+                { test: /\.md$/, loader: 'ignore-loader' },
+            ],
+        },
+        node: {
+            fs: 'empty',
+            dns: 'mock',
+            tls: 'mock',
+            child_process: 'empty',
+            dgram: 'empty',
+            zlib: 'empty',
+            __dirname: true,
+        },
+        plugins,
+    };
 };
 
 module.exports = createVariants({}, { minify: [false, true] }, createConfig);
